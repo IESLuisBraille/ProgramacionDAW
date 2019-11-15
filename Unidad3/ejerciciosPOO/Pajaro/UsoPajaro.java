d274cd4620e67fa17ed38ba62124df86f4c03364
ivanjimenez <ivan.jimenez18@educa.madrid.org>
2019-11-15 13:00:16 +0100

    .

--- Unidad3/ejerciciosPOO/Pajaro/UsoPajaro.java
+++ Unidad3/ejerciciosPOO/Pajaro/UsoPajaro.java
@@ -5,6 +5,7 @@ public class UsoPajaro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+
 		// Instanciamos la clase Pajaro para crear el objeto loro
 		Pajaro loro = new Pajaro("loro", 100, 200);
 		
@@ -14,6 +15,17 @@ public class UsoPajaro {
 		//Podemos imprimir el campo nombre, llamar a la función loro.volar(10,20) y luego imprimir posX y posY.
 		
 		System.out.printf("El %s ha volado %f y está en la posición %d,%d \n", loro.nombre, loro.volar(10, 20), loro.posX,loro.posY);
+
+		//Creamos otro objeto
+		
+		Pajaro condor = new Pajaro("condor", 300,400);
+		
+		//Seguimos el mismo proceso que con el anterior objeto
+		//Nótese que podemos hacer tantos objetos como queramos
+		
+		System.out.println(condor.nombre+ " "+condor.posX+" "+condor.posY);
+
+		System.out.printf("El %s ha volado %f y está en la posición %d,%d \n", condor.nombre, condor.volar(30, 40), condor.posX, condor.posY);
 
 	}
 
